7881fa1916590427271e2ad1a7f163c4e0dd6755
nopeitagain@mail.ru <nopeitagain@mail.ru>
2021-04-14 19:39:27 +0300

    Крыша

--- ForRepository/ForRepository.cpp
+++ ForRepository/ForRepository.cpp
@@ -5,14 +5,18 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	for (int i = 1; i < 10; i++)
+	for (int i = 0; i < 10; i++)
 	{
-		for (int j = 1; j < 10; j++)
+		for (int j = 0; j < 10; j++)
 		{
-			cout << i * j << "\t";
+			if (i + j >= 9 && i >= j)
+				cout << "|===|";
+			else
+				cout << "    ";
 		}
-		cout << "\n\n";
+		cout << endl;
 	}
+
 	return 0;
 }
 
