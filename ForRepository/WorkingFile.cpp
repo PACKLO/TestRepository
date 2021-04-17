15950217e985b4db97ebff40b78e41ce241321ef
nopeitagain@mail.ru <nopeitagain@mail.ru>
2021-04-17 17:30:59 +0300

    More programms

--- ForRepository/WorkingFile.cpp
+++ ForRepository/WorkingFile.cpp
@@ -10,9 +10,25 @@ int main()
 	cin >> Work;
 	switch (Work)
 	{
-	case 1: // две границы диапазона
+	case 1: // Проверка пользователя на таблицу умножения. Программа выводит на экран два числа, пользователь должен ввести их произведение. Имеется несколько уровней сложности. Выводит оценку знаний.
+		int fNum, sNum, composition, userInput1, possibility;
+
+		cout << "Выберите уровень сложности от 1 до 3: ";
+		cin >> possibility;
+
+
+		srand(time(NULL));
+		fNum = rand() % 100 + 1;
+		sNum = rand() % 100 + 1;
+		composition = fNum * sNum;
+		cout << composition << endl;
+		cout << fNum << endl;
+		cout << sNum << endl;
+
+
+		break;
+	case 2: // Пользователь вводит две грацицы диапазона и число. Если число не попадает в диапазон, программа просит пользователя повторно ввести число и так до тех пор пока он не введет число правльно.
 		int diapStart, diapEnd, userInput, diffirence;
-		int WorkingNumbersList[10];
 		cout << "Введите начало границы диапазона: ";
 		cin >> diapStart;
 		cout << "Введите конец границы диапазона: ";
@@ -35,6 +51,7 @@ int main()
 
 
 		break;
+
 	default:
 		cout << "Неверно! Прочтите README в репозитории!";
 		break;
