2827fae5d6f887a37265335d21e60de7ccc3db58
nopeitagain@mail.ru <nopeitagain@mail.ru>
2021-04-17 16:15:52 +0300

    More Things

---
+++ ForRepository/AnotherOne.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (i + j >= 9 && i >= j)
+				cout << "|===|";
+			else
+				cout << "    ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
